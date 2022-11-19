4f7d1dc288923631d2c65d028d2a2540de8f589e
GabrielSchiavo <gabriel.kschiavo@gmail.com>
2022-11-18 21:27:55 -0300

    Adiçao de responsividade para o app quando estiver no modo paisagem

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,23 +11,29 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                child: Text(
-                  'Nenhuma Despesa Cadastrada!',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
+        ? LayoutBuilder(
+             builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.1,
+                    child: Text(
+                      'Nenhuma Despesa Cadastrada!',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
           )
         : ListView.builder(
             itemCount: transactions.length,
--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -112,13 +113,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: const Text(
         'Despesas Pessoais',
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
       foregroundColor: Theme.of(context).colorScheme.onBackground,
-      actions: <Widget>[
+      actions: [
+        if(isLandscape)
+        IconButton(
+          icon: Icon(_showChart ? Icons.list_rounded : Icons.bar_chart_rounded),
+          onPressed: () {
+            setState(() {
+              _showChart = !_showChart;
+            });
+          },
+          color: Theme.of(context).colorScheme.onBackground,
+        ),
         IconButton(
           icon: const Icon(Icons.add_rounded),
           onPressed: () => _openTransactionFormModal(context),
@@ -134,8 +148,9 @@ class _MyHomePageState extends State<MyHomePage> {
       // ),
     );
 
-    final availableHeight =
-        MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+    final availableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
@@ -146,14 +161,36 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              SizedBox(
-                height: availableHeight * 0.22,
-                child: Chart(_recentTransactions),
-              ),
-              SizedBox(
-                height: availableHeight * 0.74,
-                child: TransactionList(_transactions, _removeTransaction),
-              ),
+              // if (isLandscape)
+              //   Row(
+              //     mainAxisAlignment: MainAxisAlignment.start,
+              //     children: [
+              //       const Text(
+              //         'Exibir Gráfico',
+              //         style: TextStyle(color: Color(0xFFE5E1E6)),
+              //       ),
+              //       Switch(
+              //         value: _showChart,
+              //         onChanged: ((value) {
+              //           setState(() {
+              //             _showChart = value;
+              //           });
+              //         }),
+              //         activeColor: Theme.of(context).colorScheme.primary,
+              //         inactiveTrackColor: Theme.of(context).colorScheme.outline,
+              //       ),
+              //     ],
+              //   ),
+              if (_showChart || !isLandscape)
+                SizedBox(
+                  height: availableHeight * (isLandscape ? 0.65 : 0.22),
+                  child: Chart(_recentTransactions),
+                ),
+              if (!_showChart || !isLandscape)
+                SizedBox(
+                  height: availableHeight * 0.73,
+                  child: TransactionList(_transactions, _removeTransaction),
+                ),
             ],
           ),
         ),
